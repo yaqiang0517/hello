32497e04a56372b32aff188581212744a53b4966
zhangyq <zhangyq@fosun.com>
2018-08-24 17:17:09 +0800

    test

---
+++ A.java
@@ -0,0 +1,13 @@
+package com.fosun.cloud.uas.config.cas;
+
+/**
+ * @ClassName A
+ * @Description TODO
+ * @Author think
+ * @Date 2018/8/2416:38
+ **/
+public class A {
+    public static void main(String[] args) {
+        System.out.println("hello dang");
+    }
+}
